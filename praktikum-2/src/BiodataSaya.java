604d73eb1f21efcbffae1bff07c29a91d37b5329
sitimaryam08 <simaryam1908@gmail.com>
2026-09-08 11:17:42 +0700

    Latihan 2 (Modifikasi Biodata saya)

--- praktikum-2/src/BiodataSaya.java
+++ praktikum-2/src/BiodataSaya.java
@@ -1,10 +1,7 @@
 public class BiodataSaya {
     public static void main(String[] args) {
-        // menampilkan nama
-        System.out.println("Nama : Siti Maryam");
-        // menampilkan nim
-        System.out.println("Nim : 2025573010026");
-        // menampilkan prodi
-        System.out.println("Program studi : Teknik Informatika");
+        System.out.print("Nama : Siti Maryam");
+        System.out.print(" - ");
+        System.out.print("Nim : 2025573010026");
     }
 }
