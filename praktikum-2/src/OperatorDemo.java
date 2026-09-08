92eba95db991e85e9165c8f458dbffaa4eb9ca7a
sitimaryam08 <simaryam1908@gmail.com>
2026-09-08 10:23:54 +0700

    Operator

--- praktikum-2/src/OperatorDemo.java
+++ praktikum-2/src/OperatorDemo.java
@@ -8,6 +8,11 @@ public class OperatorDemo {
         System.out.println("Bagi: " + (a/b));
         System.out.println("Sisa Bagi: " + (a%b));
 
+        // prktikan operator relasional dan logika
+        int nilaiUjian = 80;
+        boolean hadir = true;
+        System.out.println("Lulus (>=75): " + (nilaiUjian >= 75));
+        System.out.println("Lulus dan hadir: " + (nilaiUjian >= 75 && hadir));
 }
     
 }
