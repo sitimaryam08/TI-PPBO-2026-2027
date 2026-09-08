10096e2c91b278be26e976d1b2a14210a92bd49b
sitimaryam08 <simaryam1908@gmail.com>
2026-09-08 09:46:29 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
    public static void main(String[] args) {
-       System.out.println("Siti Maryamgit");
+       System.out.println("Siti Maryam");
+       System.out.print("Nama: ");
+       System.out.print("Budi");
+       System.out.println();
+       System.out.println("Ini baris baru");
         }
  }
