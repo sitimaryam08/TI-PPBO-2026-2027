7562d6335dd52e18150dd950e40131729a3147c3
sitimaryam08 <simaryam1908@gmail.com>
2026-09-08 11:28:11 +0700

    Latihan 3 (Data Pribadi)

---
+++ praktikum-2/src/DataPribadi.java
@@ -0,0 +1,13 @@
+public class DataSaya {
+    public static void main(String[] args) {
+        int umur = 19;
+        double tinggiBadan = 156.0;
+        char inisial = 'M';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Umur : " + umur);
+        System.out.println("Tinggi Badan : " + tinggiBadan);
+        System.out.println("Inisial : " + inisial);
+        System.out.println("Mahasiswa Aktif : " + mahasiswaAktif);
+    }
+}
