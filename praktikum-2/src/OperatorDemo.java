e00d2fb861b1ca5dfa4348fb7bd64cc65567a0cc
sitimaryam08 <simaryam1908@gmail.com>
2026-09-08 10:18:42 +0700

    Operator

---
+++ praktikum-2/src/OperatorDemo.java
@@ -0,0 +1,13 @@
+public class OperatorDemo {
+    public static void main(String[] args) {
+        int a = 17, b = 5;
+
+        System.out.println("Jumlah: " + (a+b));
+        System.out.println("Selisih: " + (a-b));
+        System.out.println("Kali: " + (a*b));
+        System.out.println("Bagi: " + (a/b));
+        System.out.println("Sisa Bagi: " + (a%b));
+
+}
+    
+}
