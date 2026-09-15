e078baf36bc6f1440662322c3783531a064bce35
sitimaryam08 <simaryam1908@gmail.com>
2026-09-15 20:12:44 +0700

    IfSederhana

---
+++ praktikum-3/src/IfDemo.java
@@ -0,0 +1,9 @@
+public class IfDemo {
+    public static void main(String[] args) {
+        int nilai = 60;
+
+        if (nilai >= 75) {
+            System.out.println("Anda Lulus");
+        }
+    }
+}
